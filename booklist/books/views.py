72949cc5b01cb2df098c84352e9b71cdc23bba01
HamiltonTD <thami583@mtroyal.ca>
2021-03-24 14:59:09 -0600

    created BookListView

--- booklist/books/views.py
+++ booklist/books/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.vies.generic import (
+from django.views.generic import (
     CreateView,
     ListView,
     UpdateView,
     DetailView,
     DeleteView,
 )
+
+from .models import Book
+
+class BookListView(ListView):
+    model = Book
+    template_name = "books/book_list.html"
+    context_object_name = "books"
+    
+    
