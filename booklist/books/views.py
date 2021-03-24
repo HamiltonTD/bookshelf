5f5a4c73ee2a64dc941491443ad787b605a4d7a5
HamiltonTD <thami583@mtroyal.ca>
2021-03-24 14:35:19 -0600

    import django generic views

--- booklist/books/views.py
+++ booklist/books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.vies.generic import (
+    CreateView,
+    ListView,
+    UpdateView,
+    DetailView,
+    DeleteView,
+)
