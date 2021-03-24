763a4f44ab4bebf4a572f9fc75130e25d89bb64f
HamiltonTD <thami583@mtroyal.ca>
2021-03-24 15:20:08 -0600

    Added remaining basic CRUD functionality

--- booklist/books/views.py
+++ booklist/books/views.py
@@ -6,6 +6,7 @@ from django.views.generic import (
     DetailView,
     DeleteView,
 )
+from django.urls import reverse_lazy
 
 from .models import Book
 
@@ -14,4 +15,24 @@ class BookListView(ListView):
     template_name = "books/book_list.html"
     context_object_name = "books"
     
+class BookCreateView(CreateView):
+    model = Book
+    fields = ('title', 'user', 'status')
+    template_name = "books/book_create.html"
+    success_url = reverse_lazy('book_list')
     
+class BookDetailView(DetailView):
+    model = Book
+    template_name = "books/book_detail.html"
+    context_object_name = "book"
+    
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ('title', 'user', 'status')
+    template_name = "books/book_update.html"
+
+    
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = "books/book_delete.html"
+    success_url = reverse_lazy('book_list')
